0367e1a707f9acbe73e043b0c1fec3f8385562df
Christian Sakai <christianmsakai@gmail.com>
2015-10-18 10:53:49 -0400

    adding test to rest api backend

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 'use strict';
 
 import mongoose from 'mongoose';
+import bluebird from 'bluebird';
 import app from './express';
-import { seedDB, mongo } from './config';
+import { seedDB, mongo } from './config/environment';
 
-// Connect to database
+// Connect to database and set up bluebird promise
 mongoose.connect(mongo.uri);
+mongoose.Promise = bluebird;
+
+// Setting up global Promise as bluebird promise
+Promise = bluebird;
 
 if (seedDB) {
-  require('./config/seed')();
+  require('./config/environment/seed')();
 }
 
 export default app;
